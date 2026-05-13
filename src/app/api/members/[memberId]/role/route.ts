8a3bec502c570b613a4b5921bdd7a3cc3b7d5572
JyrwaJR <jyrwaboys@gmail.com>
2026-05-13 23:28:07 +0530

    removing tole route

--- src/app/api/members/[memberId]/role/route.ts
+++ src/app/api/members/[memberId]/role/route.ts
@@ -45,3 +45,36 @@ export const POST = withAssociation(
     });
   },
 );
+
+export const PUT = withAssociation(
+  { body: UpdateUserRoleSchema, params: UpdateUserRoleParamsSchema },
+  async (association, { body, params }, req) => {
+    await withRole(req, UserRole.PRESIDENT);
+
+    const user = await prisma.user.findUnique({
+      where: { id: params?.memberId, associationId: association.id },
+    });
+
+    if (!user)
+      throw new NotFoundError("User does not exist in the association");
+
+    const userRole = user.role;
+
+    const removeRole = body?.role as UserRole;
+
+    if (!userRole.includes(removeRole)) {
+      throw new ConflictError("User does not have the role");
+    }
+
+    const updatedUser = await prisma.user.update({
+      where: { id: params?.memberId },
+      data: { role: userRole.filter((role) => role === removeRole) },
+      select: { id: true, role: true, email: true },
+    });
+
+    return SuccessResponse({
+      data: updatedUser,
+      message: "User role updated successfully",
+    });
+  },
+);
