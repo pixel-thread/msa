3f6a6db283f703c2838510a1da3257dd3b75fe12
JyrwaJR <jyrwaboys@gmail.com>
2026-05-20 13:35:55 +0530

    schema

--- src/features/auth/validators/index.ts
+++ src/features/auth/validators/index.ts
@@ -1,20 +1,54 @@
 import { passwordValidation } from "@src/shared/lib/validations/auth";
 import z from "zod";
 
+const associationsSlug = ["mfsa", "mpsa", "mpsc"];
+
 export const SignUpSchema = z
   .object({
-    name: z.string().min(1, "Name is required"),
     email: z.email("Invalid email address"),
-    password: passwordValidation,
-    confirm_password: passwordValidation,
-    association_slug: z
-      .enum(["mfsa", "mpsa", "mpsc"], "invalid association")
-      .optional(),
+    associationSlug: z.enum(associationsSlug, "invalid association").optional(),
+    firstName: z.string().min(3, "First name must be at least 3 characters"),
+    lastName: z.string().min(3, "Last name must be at least 3 characters"),
+    dateOfBirth: z.string("Invalid date of birth"),
+    age: z
+      .number("age must be a number")
+      .positive("age must be a positive number")
+      .gte(18, "Age must be greater than 18"),
+    // gender: z.enum(["MALE", "FEMALE", "OTHER"], "Invalid gender"),
+    address: z.string().optional(),
+    city: z.string().optional(),
+    state: z.string().optional(),
+    country: z.string().optional(),
+    postalCode: z.string().optional(),
   })
-  .refine((data) => data.password === data.confirm_password, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"],
-  });
+  .refine((val) => !Boolean(val.lastName === val.firstName), {
+    message: "First name and last name cannot be the same",
+  })
+  .refine(
+    (data) => {
+      const today = new Date();
+      const dob = new Date(data.dateOfBirth);
+
+      // 1. Calculate the rough difference in years
+      let age = today.getFullYear() - dob.getFullYear();
+
+      // 2. Adjust if the birthday hasn't happened yet this year
+      const monthDiff = today.getMonth() - dob.getMonth();
+      const dayDiff = today.getDate() - dob.getDate();
+
+      if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        age--;
+      }
+
+      // Return true if age is 18 or older
+      return age >= 18 || data.age !== age;
+    },
+    {
+      message: "You must be at least 18 years old to sign up.",
+      path: ["dateOfBirth"], // Attaches the error directly to the dateOfBirth field
+    },
+  )
+  .strict();
 
 export type SignUpInput = z.infer<typeof SignUpSchema>;
 
