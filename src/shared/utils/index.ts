92badc5789ee7a7cad4c59b70923982b69132a8f
JyrwaJR <jyrwaboys@gmail.com>
2026-05-23 15:04:30 +0530

    refactor

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -3,5 +3,5 @@ export * from "./responses";
 export * from "./tracing";
 export * from "./build-pagination";
 export * from "./format";
-export * from "./hasHighRole";
+export * from "./has-high-role";
 export * from "./http";
