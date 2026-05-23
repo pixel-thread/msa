ccf758f50722c7692fbf742101f114d107e8bf63
JyrwaJR <jyrwaboys@gmail.com>
2026-05-23 16:55:04 +0530

    update

--- src/app/api/admin/associations/route.ts
+++ src/app/api/admin/associations/route.ts
@@ -1,8 +1,4 @@
 import { withValidation, withRole } from "@src/shared/api";
-import {
-  CreateAssociationInput,
-  CreateAssociationSchema,
-} from "@validator/associations";
 import { createAssociation } from "@src/features/associations/services/createAssociation";
 import { findManyAssociation } from "@src/features/associations/services/findManyAssociation";
 import { findFirstAssociation } from "@src/features/associations/services/findFirstAssociation";
@@ -10,6 +6,8 @@ import { SuccessResponse } from "@src/shared/utils";
 import { UserRole } from "@prisma/client";
 import type { Association } from "@prisma/client";
 import { ConflictError } from "@src/shared/errors";
+import type { CreateAssociationInput } from "@validator/associations";
+import { CreateAssociationSchema } from "@src/shared/validators";
 
 export const GET = withValidation({}, async (req) => {
   await withRole(req, UserRole.SUPER_ADMIN);
