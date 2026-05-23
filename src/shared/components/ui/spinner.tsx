816057b42eff36d5a91d34a7e2de6c0422a6a3dc
JyrwaJR <jyrwaboys@gmail.com>
2026-05-23 21:30:52 +0530

    update

--- src/shared/components/ui/spinner.tsx
+++ src/shared/components/ui/spinner.tsx
@@ -1,11 +1,19 @@
-import { cn } from "@src/shared/lib/utils"
-import { HugeiconsIcon } from "@hugeicons/react"
-import { Loading03Icon } from "@hugeicons/core-free-icons"
+import { cn } from "@src/shared/lib/utils";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { Loading03Icon } from "@hugeicons/core-free-icons";
 
 function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
   return (
-    <HugeiconsIcon icon={Loading03Icon} strokeWidth={2} role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
-  )
+    <HugeiconsIcon
+      icon={Loading03Icon}
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      strokeWidth={2 as any}
+      role="status"
+      aria-label="Loading"
+      className={cn("size-4 animate-spin", className)}
+      {...props}
+    />
+  );
 }
 
-export { Spinner }
+export { Spinner };
