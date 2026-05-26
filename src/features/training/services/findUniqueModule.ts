8a64be78127bea9a2012071be16bec8997a6ed07
JyrwaJR <jyrwaboys@gmail.com>
2026-05-26 23:11:59 +0530

    update

--- src/features/training/services/findUniqueModule.ts
+++ src/features/training/services/findUniqueModule.ts
@@ -11,5 +11,10 @@ export async function findUniqueModule({
 }: FindUniqueModuleProps) {
   return await prisma.trainingModule.findUnique({
     where: { id: moduleId, associationId },
+    include: {
+      certificateTemplate: {
+        select: { id: true, certificateUrl: true, name: true },
+      },
+    },
   });
 }
