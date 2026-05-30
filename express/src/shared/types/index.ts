e24a46231567c6c7646b11e44283212ed0b2e1da
JyrwaJR <jyrwaboys@gmail.com>
2026-05-30 23:17:31 +0530

    fix issue

--- express/src/shared/types/index.ts
+++ express/src/shared/types/index.ts
@@ -2,6 +2,5 @@ export * from './api.types';
 export * from './auth.types';
 export * from './enums';
 export * from './filters';
-export * from './global.d';
 export * from './notification';
 export * from './storage';
